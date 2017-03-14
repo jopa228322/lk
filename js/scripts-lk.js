c65d884cbe16564ba037cab78b4f1fee2a5f05b6
Victor Osipov <victorosipov99@mail.ru>
2017-03-14 20:45:57 +0300

    add new page

--- js/scripts-lk.js
+++ js/scripts-lk.js
@@ -1,5 +1,49 @@
+var tabItem = $(".tab-item");
+var tab = $(".tab");
+var validAddUsl = $(".valid-r");
+var inValidAddUsl = $(".invalid-r");
+var formLcBut = $('#form-lc-but');
+var AddNewUslForm = $('.add-new-usl-form');
 
-	$(".link-lk-tours").click(function() {
-    $(this).prev()[0].click(); 
+
+
+$(validAddUsl).hide();
+$(inValidAddUsl).hide();
+
+$(".link-lk-tours").click(function() {
+  $(this).prev()[0].click(); 
 });
+	
+$(tabItem).not(":first").hide();
+$(tab).click(function(e) {
+	event.preventDefault();
+	$(tab).removeClass("active-panel-lk").eq($(this).index()).addClass("active-panel-lk");
+	$(tabItem).hide().eq($(this).index()).fadeIn()
+}).eq(0).addClass("active-panel-lk"); 
 
+
+
+$(formLcBut).on("click", function (e) {
+		e.preventDefault();
+        var $form = $(".add-new-usl-form");
+        $.ajax({
+            type: "POST",
+            url: "#",
+            data: $form.serialize(),
+        }).done(function(data) {
+        	$(validAddUsl).fadeIn();
+        	setTimeout(function(){
+        		$(inValidAddUsl).fadeOut();
+        			document.location.href='#';
+        		}, 2000);
+        }).fail(function() {
+            console.log('fail');
+        	$(inValidAddUsl).fadeIn();
+        	setTimeout(function(){
+        		$(inValidAddUsl).fadeOut();
+        	}, 2000);
+        	
+        });
+      
+        return false;
+    }); 
